e099d672989682df3ee77fa5e67afc9a76b9b770
Jared S <jaredjames2020@gmail.com>
2019-12-06 00:07:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :create_puppy
+  end
+  
+  get '/new' do
+    erb :new
+  end
+  
+  # post '/new' do
+  #   @name = params[:name]
+  #   @breed = params[:breed]
+  #   @age = params[:age]
+  # end
+  
+  post '/puppy' do
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    
+    erb :display_puppy
+  end
 
 end
